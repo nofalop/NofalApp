dae24639668c388c8a684383ea4847192fea716c
Nofal <nofalreal62@gmail.com>
2025-09-12 08:41:51 +0400

    Made UI Better

--- src/main/java/nofal/nofapp/LoginC.java
+++ src/main/java/nofal/nofapp/LoginC.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -12,22 +13,29 @@ import java.io.IOException;
 
 public class LoginC {
 	@FXML
-	private Button SignUpbtn, Loginbtn,LoginAcc;
+	private Button Signupbtn, Loginbtn,LoginAcc;
 	@FXML
 	private TextField Email;
 	@FXML
 	private PasswordField Password;
 
 	@FXML
+	private Label NameLabel,EmailLabel,PasswordLabel;
+
+	@FXML
 	public void initialize(){
 		Loginbtn.setStyle("-fx-background-color: #3498db;");
+	}
+
+	public void Login(){
+
 	}
 
 	public void SignupSwitch() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("Signup.fxml"));
 		Parent SignupRoot = loader.load();
 
-		Scene scene = SignUpbtn.getScene();
+		Scene scene = Signupbtn.getScene();
 		scene.setRoot(SignupRoot);
 	}
 }
