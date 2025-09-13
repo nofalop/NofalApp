2d76a374ac46fd5fb59c6df736c05b6282f53166
Nofal <nofalreal62@gmail.com>
2025-09-13 17:29:27 +0400

    Google GameMode

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,20 @@
 module nofal.nofapp {
+	// JavaFX
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.web;
 
+	// Extra libraries
 	requires org.controlsfx.controls;
 	requires org.kordamp.ikonli.javafx;
 	requires org.kordamp.bootstrapfx.core;
 	requires com.google.gson;
 
-	// allow both FXML loader and Gson to use reflection
-	opens nofal.nofapp to javafx.fxml, com.google.gson;
-
+	// Export packages (so other modules can use your classes)
 	exports nofal.nofapp;
+	exports nofal.nofapp.Gamemodes;
+
+	// Open packages for reflection (FXML + Gson)
+	opens nofal.nofapp to javafx.fxml, com.google.gson;
+	opens nofal.nofapp.Gamemodes to javafx.fxml;
 }
